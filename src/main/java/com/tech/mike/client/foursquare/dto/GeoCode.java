d7521cc4e2437541565f2e6c9b95cf6a95bf2789
Mike Holmes <mikeholmesuk@gmail.com>
2016-04-25 08:47:56 +0100

    Added basic validation to geo code

--- src/main/java/com/tech/mike/client/foursquare/dto/GeoCode.java
+++ src/main/java/com/tech/mike/client/foursquare/dto/GeoCode.java
@@ -2,9 +2,12 @@ package com.tech.mike.client.foursquare.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import javax.validation.constraints.NotNull;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GeoCode {
     private String what;
+    @NotNull
     private String where;
     private Feature feature;
 
